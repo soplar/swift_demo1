c03d165617de01f318315067502c2a8aba4f40cc
soplar <joan.vanduren@gmail.com>
2020-12-28 16:22:52 +0100

    Initial Commit

---
+++ Demo1/Demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo1App.swift
+//  Demo1
+//
+//  Created by soplar on 28/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
